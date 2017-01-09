e8d87bd413f93ac2c4273a90b223149b539b0d75
Jay Ullman <jayullman@gmail.com>
2017-01-08 23:09:44 -0800

    create initial boilerplate and add ajax requests

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+import requestJSON from './requestJSON';
+
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      scores_allTime: [],
+      scores_30days: []
+
+    };
+
+    // makes ajax call to FCC's server and sets the state
+    // for allTime scores and 30days scores
+    // code has been seperated into seperate module requestJSON.js
+    requestJSON.call(this);
+
+  }
+
+
+
   render() {
+
+
+
     return (
       <div className="App">
         <div className="App-header">
@@ -12,10 +36,12 @@ class App extends Component {
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          {this.state.scores_30days}
         </p>
       </div>
     );
   }
 }
+
 
 export default App;
---
+++ src/requestJSON.js
@@ -0,0 +1,41 @@
+
+
+export default function() {
+
+  // obtain JSON data for top users for all time
+  const xhr_allTimeScores = new XMLHttpRequest();
+    xhr_allTimeScores.open('Get', 'https://fcctop100.herokuapp.com/api/fccusers/top/alltime');
+
+  // obtain JSON data for top users for past 30 days
+  const xhr_recentScores = new XMLHttpRequest();
+    xhr_recentScores.open('Get', 'https://fcctop100.herokuapp.com/api/fccusers/top/recent');
+
+
+
+  xhr_allTimeScores.onreadystatechange = () => {
+    if (xhr_allTimeScores.readyState === 4) {
+      if (xhr_allTimeScores.status === 200) {
+
+      this.setState({scores_allTime: xhr_allTimeScores.responseText});
+        console.log('success');
+      } else {
+        console.log('Error: ' + xhr_allTimeScores.status);
+      }
+    }
+  };
+
+  xhr_recentScores.onreadystatechange = () => {
+    if (xhr_recentScores.readyState === 4) {
+      if (xhr_recentScores.status === 200) {
+        console.log('success');
+        console.log(xhr_allTimeScores.responseText);
+      this.setState({scores_30days: xhr_allTimeScores.responseText});
+      } else {
+        console.log('Error: ' + xhr_recentScores.status);
+      }
+    }
+  };
+
+  xhr_allTimeScores.send(null);
+  xhr_recentScores.send(null);
+}
